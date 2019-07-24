d094f8bb0db32689eb63c55c59de7c340fc6b504
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-07-24 09:45:11 +0900

    add: sponsor images

--- components/pages/top/TheSponsor.vue
+++ components/pages/top/TheSponsor.vue
@@ -4,22 +4,12 @@
         v-layout.my-4.wrap
             v-flex.md12.text-xs-center
                 first-heading(name="Sponsors")
-        v-layout.wrap.justify-start
-            v-flex.xs12.sm12.md6(v-for="i in 3" :key="i").pa-2
-                v-card
-                    v-img(src="https://via.placeholder.com/500" height="100")
-        v-layout.wrap.justify-start
-            v-flex.xs12.sm12.md4(v-for="i in 10" :key="i").pa-2
-                v-card
-                    v-img(src="https://via.placeholder.com/500" height="100")
-        v-layout.wrap.justify-start
-            v-flex.xs12.sm12.md3(v-for="i in 10" :key="i").pa-2
-                v-card
-                    v-img(src="https://via.placeholder.com/500" height="100")
-        v-layout.wrap.justify-start
-            v-flex.xs12.sm12.md2(v-for="i in 20" :key="i").pa-2
-                v-card
-                    v-img(src="https://via.placeholder.com/500" height="100")
+        h3.text-xs-center.display-1 Diamond
+        sponsors-group(:sponsors="diamondSponsors" :xs="12" :sm="6" :md="4")
+        h3.text-xs-center.display-1 Platinum
+        sponsors-group(:sponsors="platinumSponsors" :xs="8" :sm="4" :md="3")
+        h3.text-xs-center.display-1 Gold
+        sponsors-group(:sponsors="goldSponsors" :xs="6" :sm="3" :md="2")
         v-layout.my-3.wrap.justify-center
             v-flex.xs12.sm12.md3.text-xs-right.px-2
                 v-btn(outline block color="primary" round large)
@@ -33,10 +23,36 @@
 
 <script>
 import FirstHeading from "@/components/parts/FirstHeading"
+import SponsorsGroup from "@/components/parts/SponsorsGroup"
+import sponsorsData from "@/assets/sponsors"
+
 export default {
-    name: "TheSupport",
+    name: "TheSponsor",
     components: {
         "first-heading": FirstHeading,
+        "sponsors-group": SponsorsGroup,
+    },
+    data() {
+        return {
+            sponsors: sponsorsData,
+        }
+    },
+    computed: {
+        diamondSponsors() {
+            return this.sponsors.filter(sponsor => sponsor.package === "Diamond")
+        },
+        platinumSponsors() {
+            return this.sponsors.filter(sponsor => sponsor.package === "Platinum")
+        },
+        goldSponsors() {
+            return this.sponsors.filter(sponsor => sponsor.package === "Gold")
+        },
+        sprintSponsors() {
+            return this.sponsors.filter(sponsor => sponsor.package === "Sprint Sponsorship")
+        },
+        additionalSponsors() {
+            return this.sponsors.filter(sponsor => sponsor.package === "Additional Plans")
+        }
     }
 }
 </script>
---
+++ components/parts/SponsorsGroup.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+v-layout.wrap.justify-center.pb-4
+    v-flex(v-for="sponsor in sponsors" :key="sponsor.id" :class="[`xs${xs}`, `sm${sm}`, `md${md}`]").pa-2
+        v-card.pa-2
+            v-img(v-if="sponsor.logoIImage" :src="require(`@/assets/images/logos/${sponsor.logoIImage}`)" :aspect-ratio="1" contain)
+            v-responsive(v-else :aspect-ratio="1")
+                v-layout(align-center justify-center row fill-height)
+                        v-card-text
+                            p.text-xs-center.font-weight-bold.small {{sponsor.name}}
+
+</template>
+
+<script>
+export default {
+  props: {
+    sponsors: {
+      type: Array,
+      required: true
+    },
+    xs: {
+      type: Number,
+      required: true
+    },
+    sm: {
+      type: Number,
+      required: true
+    },
+    md: {
+      type: Number,
+      required: true
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+    margin: 0;
+}
+
+.small {
+    font-size: 10px;
+}
+</style>
