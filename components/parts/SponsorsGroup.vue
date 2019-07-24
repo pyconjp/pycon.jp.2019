36bf4b1394b7389a093d79225aacddd6d9ab6f03
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2019-07-24 21:10:28 +0900

    Do minor adjustment on logo image

--- components/parts/SponsorsGroup.vue
+++ components/parts/SponsorsGroup.vue
@@ -1,12 +1,16 @@
 <template lang="pug">
 v-layout.wrap.justify-center.pb-4
     v-flex(v-for="sponsor in sponsors" :key="sponsor.id" :class="[`xs${xs}`, `sm${sm}`, `md${md}`]").pa-2
-        v-card.pa-2
+        v-card(
+          :class="{'pa-2': $vuetify.breakpoint.smAndDown, 'pa-4': $vuetify.breakpoint.mdAndUp}"
+        )
             v-img(v-if="sponsor.logoIImage" :src="require(`@/assets/images/logos/${sponsor.logoIImage}`)" :aspect-ratio="3/2" contain)
             v-responsive(v-else :aspect-ratio="3/2")
                 v-layout(align-center justify-center row fill-height)
                         v-card-text
-                            p.text-xs-center.font-weight-bold.small {{sponsor.name}}
+                            p.text-xs-center.font-weight-bold(
+                              :class="{'small': $vuetify.breakpoint.smAndDown, 'headline': $vuetify.breakpoint.mdAndUp}"
+                            ) {{sponsor.name}}
 
 </template>
 
