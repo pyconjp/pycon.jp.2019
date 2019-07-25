82d1500eb8a1cdc30d0c43aaf6e84877505088c3
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2019-07-25 13:52:56 +0900

    Add circleci and functions for basic auth

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions')
+const basicAuth = require('basic-auth-connect')
+const express = require('express')
+
+const app = express()
+
+if(process.env.GCLOUD_PROJECT === "pyconjp-2019-dev"){
+    app.all('/*', basicAuth(function(user, password) {
+        return user === 'pycon' && password === 'pyconpycon';
+    }));
+}
+
+app.use(express.static(__dirname + '/dist/'))
+
+exports.app = functions.https.onRequest(app)
