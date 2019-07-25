6be3b7bd8a8e92f7165312b728471e1d85927c39
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2019-07-25 17:46:34 +0900

    Add news api

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions')
+
+/* Hosting */
+
 const basicAuth = require('basic-auth-connect')
 const express = require('express')
 
@@ -13,3 +16,18 @@ if(process.env.GCLOUD_PROJECT === "pyconjp-2019-development"){
 app.use(express.static(__dirname + '/dist/'))
 
 exports.app = functions.https.onRequest(app)
+
+/* Functions */
+
+const axios = require("axios")
+const { parseString } = require('xml2js')
+
+exports.news = functions.https.onRequest(async (req, res) => {
+    const maxResult = req.query.maxResult || 5
+    const url = `http://pyconjp.blogspot.com/feeds/posts/default/-/pyconjp2018?alt=rss&max-results=${maxResult}`
+    const resp = await axios.get(url)
+    parseString(resp.data, (err, result) => {
+        const posts = result.rss.channel
+        res.json(posts)
+    })
+});
