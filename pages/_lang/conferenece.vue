c430a2f6c530e69b93ad7b344175889629b87b6c
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-08-19 23:49:09 +0900

    add: plain text -> local text

--- pages/_lang/conferenece.vue
+++ pages/_lang/conferenece.vue
@@ -4,35 +4,31 @@
     event-header(day16 day17 :image="require('@/assets/images/photo_image2.png')" applyLink="#" place="大田区産業プラザPiO")
     section.abstract
         v-container.py-0
-            p 私たちは、Pythonユーザーが一堂に会し、互いに交流を深め、知識を分けあい、新たな可能性を見つけられる場所として、PyCon JPの企画と運営に取り組んできました。とりわけ、PyCon JPを広く世界のPythonユーザにも開かれた場と位置づけ、国内外のユーザにとって魅力的なカンファレンスとなるよう努力をつづけています。
-            p PyCon JPを素晴らしいカンファレンスとして維持していくには、ただ自分たちの会を毎年同じ時期に同じ場所で開催するだけでは足りないと、私たちは考えます。国内外で行われるカンファレンスとの連携や、ユーザー間の交流の促進、Pythonや関連するソフトウェアの普及・啓蒙をお手伝いしていくことで、Pythonにもっと多くの関心が集まり、沢山のユーザやソフトウェアが生まれていくことが、私たちの原動力となると信じています。
-            p PyCon JP 2019今年のテーマはPython New Eraです。日本で元号が新しくなった事。Pythonも2から3へ新しい時代へ移っていく事をコンセプトとしています。
-                br
-                | PyCon JP 2019は参加する皆さんが新しい繋がりや知識を得て楽しめるカンファレンスを目標に開催します。
+            p {{ $t("home.conference.main") }}
             .text-xs-center.text-sm-left
-                button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") セッション一覧
-                button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") タイムテーブル
+                button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") {{ $t("home.conference.sessions") }}
+                button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") {{ $t("home.conference.timetables") }}
     event-page-index(:indexes="pageIndexes")
     section.summary
         v-container.py-0
-            hs カンファレンスの開催概要
+            hs {{ $t("conference.sections.summary") }}
             .summary__schedule
-                h3 日程
+                h3 {{ $t("basic.schedule") }}
                 schedule-table(day16 day17 day16Time="09:30 - 20:30" day17Time="09:30 - 19:00")
-                    template(#day16-detail) ※昼食はお弁当が出ます。またおやつの提供と、18:00よりアフターパーティがあります。
-                    template(#day17-detail) ※昼食はお弁当が出ます。またおやつの提供があります。
+                    template(#day16-detail) {{ $t('events.lunch_oyatsu_party') }}
+                    template(#day17-detail) {{ $t('events.lunch_oyatsu') }}
             .summary__place
-                h3 会場
-                p.font-weight-bold 大田区産業プラザ PiO
-                p 〒144-0035　東京都大田区南蒲田一丁目20番20号
+                h3 {{ $t('basic.access') }}
+                p.font-weight-bold {{ $t('basic.pio') }}
+                p {{ $t('basic.address.pio') }}
             iframe.google-map(src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3245.7950378596533!2d139.7219626156352!3d35.55876384428529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601860f87f5da4e3%3A0x8a0493a2f4accfb0!2z5aSn55Sw5Yy655Sj5qWt44OX44Op44K2UGlP!5e0!3m2!1sja!2sjp!4v1566138857137!5m2!1sja!2sjp" width="100%" height="320" frameborder="0" style="border:0" allowfullscreen)
     section.keynote
         v-container.py-0
-            hs(sub="基調講演") Keynote
+            hs(:sub="$t('conference.sections.keynote')") Keynote
             two-columns-layout
                 template(#left)
                     schedule-table(day16 day16Time="00:00 - 00:00")
-                    p 開催場所（ホール名など）テキストテキスト
+                    p {{ $t('events.place') }} {{ $t('conference.place.keynote_day16') }}
                     talk-card(
                         profileImg="cory_althoff.png"
                         speakerName="Cory Althoff"
@@ -41,7 +37,7 @@
                     )
                 template(#right)
                     schedule-table(day17 day17Time="00:00 - 00:00")
-                    p 開催場所（ホール名など）テキストテキスト
+                    p {{ $t('events.place') }} {{ $t('conference.place.keynote_day17') }}
                     talk-card(
                         profileImg="koike.png"
                         speakerName="小池 誠"
@@ -50,9 +46,9 @@
                     )
     section.invited-lecture
         v-container.py-0
-            hs(sub="招待講演") Invited Lecture
+            hs(:sub="$t('conference.sections.invited_lecture')") Invited Lecture
             schedule-table(day17 day17Time="00:00 - 00:00")
-            p 開催場所(ホールなど) テキストテキスト
+            p {{ $t('events.place') }} {{ $t('conference.place.invited_lecture') }}
             talk-card(
             profileImg="lyu.png"
             speakerName="Renyuan Lyu"
@@ -61,79 +57,71 @@
             )
     section.talk-session
         v-container.py-0
-            hs(sub="トークセッション") Talk Session
+            hs(:sub="$t('conference.sections.talk_session')") Talk Session
             two-columns-layout
                 template(#left)
                     v-img(max-height="20vh" :src="require('@/assets/images/Photo5.png')")
                 template(#right)
-                    p 説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
-                    p 説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
-                    button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") トークセッション一覧
+                    p {{ $t('conference.talk_session') }}
+                    button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") {{ $t('conference.talk_session_list') }}
     section.lightning-talk
         v-container.py-0
-            hs(sub="ライトニングトーク") Lightning Talk
+            hs(:sub="$t('conference.sections.lightning_talk')") Lightning Talk
             two-columns-layout(sm-reverse)
                 template(#left)
-                    p
-                        | 5分間で行う発表（プレゼンテーション）のことです。「Lightning Talk（稲妻トーク）」を略して「LT」とも呼びます。
-                        br
-                        | 楽しくても5分、つまらなくても5分。より多くの人に発表する機会をもってもらうための企画です。Pythonに関係あっても無くてもOKです！
-                    p クロージングLTは、カンファレンス最後に参加者全員が、メイン会場に集まって行っています。発表者は全員の前で発表ができ、聞く人も含めた全ての参加者が一緒に、イベントを盛り上げます。今年は、どんなLTが発表されるのか楽しみですね。みんなで盛り上げましょう！
-                    button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") ライトニングトーク一覧
+                    p {{ $t('conference.lightning_talk') }}
+                    button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") {{ $t('conference.lightning_talk_list') }}
                 template(#right)
                     v-img(max-height="20vh" :src="require('@/assets/images/lightning-talk.png')")
             .event-detail.mt-5.pa-5
-                h3 日時・場所
+                h3 {{ $t('basic.access_schedule') }}
                 schedule-table(day16 day17 day16Time="00:00 - 00:00 （n分間）　枠数：n枠" day17Time="00:00 - 00:00 （n分間）　枠数：n枠")
-                p 開催場所（ホール名など）テキストテキスト
-                h3 LT発表希望者の方へ
-                p 当日朝に写真のような形でホワイトボードに名前を書いてもらう方式で募集します。早い者勝ちで行うので、LTやるぞ！という方は早目に会場入りをしてください。LTの時間は5分で、枠数は1日目8枠、2日目7枠を予定してます。
-                p.text2--text ※日本語がわからない人もある程度楽しめるように、スライドは英語で作ってください。
+                p {{ $t('events.place') }} {{ $t('conference.place.lightning_talk') }}
+                h3 {{ $t('conference.for_LTer') }}
+                p {{ $t('conference.for_LTer_main') }}
+                p.text2--text {{ $t('conference.for_LTer_sub') }}
     section.beginners-session
         v-container.py-0
-            hs(sub="ビギナーセッション") Beginner's Session
+            hs(:sub="$t('conference.sections.beginners_session')") Beginner's Session
             two-columns-layout
                 template(#left)
                     v-img(max-height="20vh" :src="require('@/assets/images/beginners-session.png')")
                 template(#right)
-                    p 説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
-                    p 説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
+                    p {{ $t('conference.begginer_session') }}
             .event-detail.pa-5
-                h3 日時・場所
+                h3 {{ $t('basic.access_schedule') }}
                 schedule-table(day16 day16Time="00:00 - 00:00 （n分間）")
-                p 開催場所（ホール名など）テキストテキスト
-                h3 参加対象
-                p 説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
+                p {{ $t('events.place') }} {{ $t('conference.place.begginers_session') }}
+                h3 {{ $t('events.target') }}
+                p {{ $t('conference.begginer_session_target') }}
     section.poster-indivisual
         v-container.py-0
-            hs(sub="ポスターセッション(個人)" tail="Indivisual") Poster Session
+            hs(:sub="$t('conference.sections.poster_indivisual')" tail="Indivisual") Poster Session
             two-columns-layout(sm-reverse)
                 template(#left)
-                    p 説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
-                    p 説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
-                    button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") ポスターセッション一覧
+                    p {{ $t('conference.poster_indivisual') }}
+                    button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") {{ $t('conference.posters') }}
                 template(#right)
                     v-img(max-height="20vh" :src="require('@/assets/images/poster-indivisual.png')")
     section.poster-community
         v-container.py-0
-            hs(sub="ポスターセッション(コミュニティ)" tail="Community") Poster Session
+            hs(:sub="$t('conference.sections.poster_community')" tail="Community") Poster Session
             two-columns-layout
                 template(#left)
                     v-img(max-height="20vh" :src="require('@/assets/images/poster-community.png')")
                 template(#right)
-                    p 説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
-                    p 説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
-                    button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") ポスターセッション一覧
+                    p {{ $t('conference.poster_community') }}
+                    button-with-arrow(color="themeColor1" outline :dark="true" :block="false"  href="#") {{ $t('conference.posters') }}
     supports
     section.after-party
         v-container.py-0
-            hs(sub="アフターパーティ") After Party
+            hs(:sub="$t('conference.sections.after_party')") After Party
             two-columns-layout(sm-reverse)
                 template(#left)
-                    p カンファレンスの終わりにアフターパーティが～…参加情報や注意点など説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
-                    h3 日時・場所
+                    p {{ $t('conference.after_party') }}
+                    h3 {{ $t('basic.access_schedule') }}
                     schedule-table(day16 day16Time="00:00 - 00:00")
-                    p 開催場所（ホール名など）テキストテキスト
+                    p {{ $t('events.place') }} {{ $t('conference.place.after_party') }}
                 template(#right)
                     v-img(:src="require('@/assets/images/after-party.png')")
 
@@ -165,15 +153,15 @@ export default {
   data() {
     return {
       pageIndexes: [
-        'カンファレンスの開催概要',
-        '基調講演',
-        '招待講演',
-        'トークセッション',
-        'ビギナーズセッション',
-        'ポスターセッション(個人)',
-        'ポスターセッション(コミュニティ)',
-        'サポート：食事、託児所、遠方支援',
-        'アフターパーティー'
+        this.$t('conference.sections.summary'),
+        this.$t('conference.sections.keynote'),
+        this.$t('conference.sections.invited_lecture'),
+        this.$t('conference.sections.talk_session'),
+        this.$t('conference.sections.lightning_talk'),
+        this.$t('conference.sections.beginners_session'),
+        this.$t('conference.sections.poster_indivisual'),
+        this.$t('conference.sections.poster_community'),
+        this.$t('conference.sections.after_party')
       ]
     }
   }
