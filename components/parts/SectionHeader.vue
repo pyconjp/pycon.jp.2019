6f11b633146ef618c461c2103f0fc436b1a52b6b
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-08-19 16:18:07 +0900

    add: SectionHeader Component

---
+++ components/parts/SectionHeader.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+.section-header
+    h2.section-header__main
+        slot
+        .section-header__tail(v-if="tail") - {{ tail }}
+    p.section-header__sub.blueGrey1--text.font-weight-bold {{ sub }}
+</template>
+
+<script>
+export default {
+  props: {
+    sub: {
+      type: String
+    },
+    tail: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
+@import '@/assets/style/custom.sass';
+
+.section-header {
+  &__main {
+    @extend .heading-font;
+
+    font-size: 28px;
+    vertical-align: middle;
+    margin: 24px 0 0;
+    margin-left: -60px;
+
+    &::before {
+      content: '';
+      display: inline-block;
+      height: 4px;
+      width: 40px;
+      margin-right: 20px;
+      background-color: $themeColor3;
+    }
+  }
+
+  &__tail {
+    display: inline-block;
+    font-weight: lighter;
+    font-size: 28px;
+    padding-left: 0.5em;
+  }
+}
+</style>
