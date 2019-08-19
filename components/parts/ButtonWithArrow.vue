68ddce3b4ce61e129ae27b2fcd7d889fa2d7a927
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-08-19 16:16:45 +0900

    update: un-block button support

--- components/parts/ButtonWithArrow.vue
+++ components/parts/ButtonWithArrow.vue
@@ -3,38 +3,47 @@ v-btn(
      :block="block" :color="color" :[size]="true" :outline="outline" round :style="customStyle" :href="href" :target="target"
      :disabled="disabled" :dark="dark" :flat="flat"
 ).parent
-    span.font-weight-bold
+    span.font-weight-bold(:class="{'is-not-block': !block}")
         slot
     v-icon(:[size]="true").child keyboard_arrow_right
 </template>
 
 <style lang="sass" scoped>
+@import '@/assets/style/variables.scss'
+
 .parent
     position: relative
 .child
     position: absolute
     right: -10px
-@media only screen and (min-width: 1264px)
+.is-not-block
+    padding: 0 2em
+@media #{$display-breakpoint-lg-and-up}
     .child
-        right: 0px
+        right: -.5em
+    .is-not-block
+        padding: 0 3em
+
+.v-btn.v-btn--outline
+    border-width: 2px
 </style>
 
 
 <script>
 export default {
-    name: "button-with-arrow",
-    props: {
-        color: {tyep: String},
-        size: {type: String, default: "large"},
-        customStyle: {type: String},
-        href: {type: String},
-        block: {type: Boolean, default: true},
-        outline: {type: Boolean, default: false},
-        disabled: {type: Boolean, default: false},
-        dark: {type: Boolean, default: false},
-        target: {type: String, default: "_self"},
-        flat: {type: Boolean, default: false}
-    }
+  name: 'button-with-arrow',
+  props: {
+    color: { tyep: String },
+    size: { type: String, default: 'large' },
+    customStyle: { type: String },
+    href: { type: String },
+    block: { type: Boolean, default: true },
+    outline: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false },
+    dark: { type: Boolean, default: false },
+    target: { type: String, default: '_self' },
+    flat: { type: Boolean, default: false }
+  }
 }
 </script>
 
