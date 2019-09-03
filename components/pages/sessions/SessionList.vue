a38b88a5395bf11d41820945fe922eb909e5d77d
Naoki Saruta <katsusand1@gmail.com>
2019-09-03 11:47:59 +0900

    GH-49 sessionsページの作成
    * sessionsページの作成
       * SessionModalWindowコンポーネントの作成
       * sessionsページ用の語彙の追加

---
+++ components/pages/sessions/SessionList.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+ul.pl-0.mt-4.mb-5
+  li.py-4(v-for="session in sessions")
+    SessionCard(:session="session")
+</template>
+
+<script>
+  import SessionCard from '@/components/pages/sessions/SessionCard'
+
+  export default {
+    props: {
+      sessions: {
+        type: Array,
+        required: true
+      },
+    },
+    components: {
+      SessionCard,
+    }
+  }
+</script>
+
+<style lang="scss">
+@import '@/assets/style/variables.scss';
+
+ul {
+  list-style: none;
+  li {
+    border-bottom: 1px solid $themeColor3Transparent;
+  }
+}
+</style>
--- pages/_lang/sessions.vue
+++ pages/_lang/sessions.vue
@@ -1,20 +1,26 @@
 <template lang="pug">
-#timetable
+#sessions
     page-header(ja="セッションリスト" en="Sessions")
     v-container
-      v-layout.my-4
-        v-flex.text-md-left.font-weight-bold {{ $t("timetable.note") }}
-      v-layout.tabs.timetable
+      v-layout.tabs.sessions
         v-flex.text-md-center.tab(:class="activeTab === 0 ? 'active' : ''")
           v-btn(block flat @click="activeTab = 0")
-            .headline 1日目 09.16 （月・祝）
+            .headline {{ $t('sessions.session_category.talk_session') }}
         v-flex.text-md-center.tab(:class="activeTab === 1 ? 'active' : ''")
           v-btn(block flat @click="activeTab = 1")
-            .headline 2日目 09.17 （火）
+            .headline {{ $t('sessions.session_category.lightning_talk') }}
+        v-flex.text-md-center.tab(:class="activeTab === 2 ? 'active' : ''")
+          v-btn(block flat @click="activeTab = 2")
+            .headline {{ $t('sessions.session_category.poster_session') }}
       v-layout.tab-item(v-if="activeTab === 0")
-        v-flex 16 text
+        v-flex
+          SessionList(:sessions="talkSessions")
       v-layout.tab-item(v-if="activeTab === 1")
-        v-flex 17 text
+        v-flex
+          SessionList(:sessions="lightningTalks")
+      v-layout.tab-item(v-if="activeTab === 2")
+        v-flex
+          SessionList(:sessions="posterSessions")
 </template>
 
 <script>
@@ -30,6 +36,8 @@ import EventHeader from '@/components/parts/EventHeader'
 import EventPageIndex from '@/components/parts/EventPageIndex'
 import Supports from '@/components/parts/Supports'
 
+import SessionList from '@/components/pages/sessions/SessionList'
+
 export default {
   components: {
     PageHeader,
@@ -40,15 +48,45 @@ export default {
     ScheduleTable,
     EventHeader,
     EventPageIndex,
-    Supports
+    Supports,
+    SessionList
   },
   data() {
     return {
-        sessions: sessions
+      sessions: sessions,
+      activeTab: 0
     }
   },
-  created() {
-      console.log(this.sessions)
+  computed: {
+    talkSessions() {
+      return this.sessions.filter(session => session.talk_format_origin.match(/^Talk/))
+    },
+    lightningTalks() {
+      return this.sessions.filter(session => session.talk_format_origin.match(/^Lightning/))
+    },
+    posterSessions() {
+      return this.sessions.filter(session => session.talk_format_origin.match(/^Poster/))
+    },
   }
 }
 </script>
+
+<style lang="sass">
+.sessions .v-ripple__container
+  display: none
+</style>
+
+<style lang="sass" scoped>
+@import '@/assets/style/variables.scss'
+
+.tab
+  border-bottom: solid $blueGrey2 1px
+.tab.active
+  border-right: solid $blueGrey2 1px
+  border-left: solid $blueGrey2 1px
+  border-top: solid $blueGrey2 1px
+  border-bottom: none
+.v-btn:before
+  display: none
+
+</style>
