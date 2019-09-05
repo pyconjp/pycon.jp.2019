cd86f5744c0caac9b3ba25daa912ac946588be47
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2019-09-05 14:57:26 +0900

    Fix bugs

--- layouts/default.vue
+++ layouts/default.vue
@@ -40,9 +40,9 @@ export default {
           name: this.$t('header.menu.about.parent'),
           to: this.$router.history.base + '/about',
           submenus: [
-            { 
-              id: 0, name: this.$t('header.menu.about.about-event'), to: this.$router.history.base + "/#overview" 
-            },
+            // { 
+            //   id: 0, name: this.$t('header.menu.about.about-event'), to: this.$router.history.base + "/#overview" 
+            // },
             { 
               id: 1, name: this.$t('header.menu.about.coc'), pageTrans: false, to: this.$router.history.base + "/code-of-conduct" 
             }
