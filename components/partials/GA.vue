49aa805450b0770a7bf12a17c4ff7b7960494f31
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2019-09-14 17:40:18 +0900

    Add GA tag

---
+++ components/partials/GA.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="ga">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148004196-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-148004196-1');
+    </script>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+    }
+}
+</script>
+
