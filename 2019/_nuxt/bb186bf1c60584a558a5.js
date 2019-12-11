503d4a3a301a72dd6ed9ebf42481bcc81447e8d3
Ikeda Yutao <yutaro.ikeda@kaizenplatform.com>
2019-12-11 17:36:52 +0900

    Add static files

---
+++ 2019/_nuxt/bb186bf1c60584a558a5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{641:function(n,l,o){"use strict";o.r(l);var e=o(631).default,t=o(2),component=Object(t.a)(e,void 0,void 0,!1,null,null,null);l.default=component.exports}}]);
